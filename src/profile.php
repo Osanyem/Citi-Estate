68faf74e9c9502d3c7500c0af3f1e8babe3b4492
Osanyem Osadebe <osanyemo@gmail.com>
2024-12-24 14:44:32 -0500

    fix profile

--- src/profile.php
+++ src/profile.php
@@ -16,12 +16,13 @@ session_start(); ?>
   <link href="css/bootstrap.min.css" rel="stylesheet">
 
 
-<style type="text/css">
-
-	   		#navbar	{position: sticky;	top:0px; z-index: 1}
-
-	
-</style>  
+  <style type="text/css">
+    #navbar {
+      position: sticky;
+      top: 0px;
+      z-index: 1
+    }
+  </style>
 
 
 
@@ -30,53 +31,56 @@ session_start(); ?>
 
 <body style="background-color: rgb(248,249,250);">
 
-<?php 
-require ('forms/user.php');
-$r = new User;
-require('components/nav-bar.php');
+  <?php
+  require('forms/user.php');
+  $r = new User;
+  require('components/nav-bar.php');
 
-if(!isset($_SESSION['userid'])){header('location:index.php');}
+  if (!isset($_SESSION['userid'])) {
+    header('location:index.php');
+  }
 
-$details = $r->getDetails($_SESSION['userid']);
-if (!empty($details)) {
+  $details = $r->getDetails($_SESSION['userid']);
+  if (!empty($details)) {
 
- ?>
+    ?>
 
-  <!-- Page Content -->
-  <div class="container">
+    <!-- Page Content -->
+    <div class="container">
 
-    <!-- Page Heading/Breadcrumbs -->
-    <h1 class="mt-4 mb-3">Profile Page</h1>
+      <!-- Page Heading/Breadcrumbs -->
+      <h1 class="mt-4 mb-3">Profile Page</h1>
 
-    <div class='alert alert-info'> You joined Citi Estate on <?php echo $details['date_joined']; ?></div>
+      <div class='alert alert-info'> You joined Citi Estate on <?php echo $details['date_joined']; ?></div>
 
-    <!-- Content Row -->
-    <div class="row">
-      <!-- Sidebar Column -->
-      <div class="col-lg-3 mb-4">
-	
-        <div class="list-group">
-          <a href="index.php" class="list-group-item">Home</a>
-          <a href="edit_profile.php" class="list-group-item">Edit Profile</a>
-          <a href="properties.php" class="list-group-item">List Property</a>
-          <a href="manage_prop.php" class="list-group-item">Manage Properties</a>
-          <a href="forms/logout.php" class="list-group-item">Logout</a>
-         
-          
+      <!-- Content Row -->
+      <div class="row">
+        <!-- Sidebar Column -->
+        <div class="col-lg-3 mb-4">
+
+          <div class="list-group">
+            <a href="index.php" class="list-group-item">Home</a>
+            <a href="edit_profile.php" class="list-group-item">Edit Profile</a>
+            <a href="properties.php" class="list-group-item">List Property</a>
+            <a href="manage_prop.php" class="list-group-item">Manage Properties</a>
+            <a href="forms/logout.php" class="list-group-item">Logout</a>
+
+
+          </div>
+        </div>
+        <!-- Content Column -->
+        <div class="col-lg-9 mb-4">
+          <h2>Hi <?php echo $details['fname'] . ' ' . $details['lname']; ?>,</h2>
+          <p>This is your Citi Estate profile page, from here you can upload properties and manage properties which you
+            have you uploaded to Citi Estate.</p>
         </div>
       </div>
-      <!-- Content Column -->
-      <div class="col-lg-9 mb-4">
-        <h2>Hi <?php echo $details['fname'] . ' ' . $details['lname'];?>,</h2>
-        <p>This is your Citi Estate profile page, from here you can upload properties and manage properties which you have you uploaded to Citi Estate.</p>
-      </div>
-    </div>
-    <!-- /.row -->
+      <!-- /.row -->
 
-  
-  <!-- /.container -->
 
-     <hr>
+      <!-- /.container -->
+
+      <hr>
       <div class="row">
         <div class="col-sm-12 col-md-10 offset-md-1" style="text-align:center">
           <a href="" style="padding:30px">About</a>
@@ -89,7 +93,10 @@ if (!empty($details)) {
       <hr>
       <div class="row">
         <div class="col-sm-12 col-md-8 offset-md-2">
-        <p style="text-align:center; font-size:0.7em">Citi Estate is committed to ensuring digital accessibility for individuals with disabilities. We are continuously working to improve the accessibility of our web experience for everyone, and we welcome feedback and accommodation requests. If you wish to report an issue or seek an accommodation, please <a href="">contact us.</a></p>
+          <p style="text-align:center; font-size:0.7em">Citi Estate is committed to ensuring digital accessibility for
+            individuals with disabilities. We are continuously working to improve the accessibility of our web experience
+            for everyone, and we welcome feedback and accommodation requests. If you wish to report an issue or seek an
+            accommodation, please <a href="">contact us.</a></p>
         </div>
       </div>
       <div class="row">
@@ -101,13 +108,14 @@ if (!empty($details)) {
           <span style="color: gray"><i>&copy; Est: 2019</i></span>
         </div>
       </div>
-      </div>
-<?php }
-else{ header('location:index.php'); }
-?>
-<script src='js/jquery.js' type='text/javascript'></script>
-<script src='js/popper.min.js' type='text/javascript'></script>
-<script src='js/bootstrap.min.js' type='text/javascript'></script>
+    </div>
+  <?php } else {
+    header('location:index.php');
+  }
+  ?>
+  <script src='js/jquery.js' type='text/javascript'></script>
+  <script src='js/popper.min.js' type='text/javascript'></script>
+  <script src='js/bootstrap.min.js' type='text/javascript'></script>
 
 </body>
 
